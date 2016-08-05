4190a71e382de2a931039defb2f8f287eb0eb2aa
bergenhem <cbergenhem@gmail.com>
2016-08-04 22:25:20 -0400

    Add CSS for form header and button

--- scripts/components/Travel.js
+++ scripts/components/Travel.js
@@ -7,17 +7,6 @@ const Travel = React.createClass({
   },
   reasonChange: function(event) {
     this.setState({ travelReason: event.target.value });
-
-
-    // $('.switch label').on('click', function(){
-    //   var indicator = $(this).parent('.switch').find('span');
-    //   if ( $(this).hasClass('right') ){
-    // 		$(indicator).addClass('right');
-    //   } else {
-    //     $(indicator).removeClass('right');
-    //   }
-    // });
-
   },
   changeType: function(event) {
     var currentType = event.target.type;
@@ -34,36 +23,43 @@ const Travel = React.createClass({
     console.log("add item");
     console.log(event.target);
   },
+  // Design for the form inspired by: http://codepen.io/colorlib/pen/rxddKy
+  // Design for the switch inspired by: http://codepen.io/kylephillips/pen/MYwXqV
   render() {
     return(
       <div className="travelArea">
-        <div className="radioButtonArea">
-          <div className="switch">
-            <div className="travelRadioButton">
-              <input id="workOption" type="radio" name="travelType" value="work"
-                checked={ this.state.travelReason === "work" }
-                onChange={ this.reasonChange } />
-              <label htmlFor="workOption">Work</label>
+        <div className="travelHeader">
+          <h1>Add Travel</h1>
+        </div>
+        <div className="travelContentArea">
+          <div className="radioButtonArea">
+            <div className="switch">
+              <div className="travelRadioButton">
+                <input id="workOption" type="radio" name="travelType" value="work"
+                  checked={ this.state.travelReason === "work" }
+                  onChange={ this.reasonChange } />
+                <label htmlFor="workOption">Work</label>
+              </div>
+              <div className="travelRadioButton">
+                <input id="personalOption" type="radio" name="travelType" value="personal"
+                  checked={ this.state.travelReason === "personal" }
+                  onChange={ this.reasonChange } />
+                <label htmlFor="personalOption">Personal</label>
+              </div>
+              <span className={ (this.state.travelReason != "work") ? "right" : null } aria-hidden="true"></span>
             </div>
-            <div className="travelRadioButton">
-              <input id="personalOption" type="radio" name="travelType" value="personal"
-                checked={ this.state.travelReason === "personal" }
-                onChange={ this.reasonChange } />
-              <label htmlFor="personalOption">Personal</label>
-            </div>
-            <span className={ (this.state.travelReason != "work") ? "right" : null } aria-hidden="true"></span>
           </div>
+          {/* This is the personal-related input area */}
+          <div className="dateInputArea">
+            <input id="startDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="Start Date" />
+            <input id="endDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="End Date" />
+          </div>
+          {/* This is the work-related input area */}
+          <div className={ (this.state.travelReason != "work") ? "hidden" : null }>
+            <input id="companyInput" type="text" placeholder="Company Name"/>
+          </div>
+          <button className="submitTravelButton" onClick={ this.submitTravel }>Add Travel</button>
         </div>
-        {/* This is the personal-related input area */}
-        <div className="dateInputArea">
-          <label htmlFor="startDate">Start Date</label><input id="startDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="Start Date" />
-          <label htmlFor="endDate">End Date</label><input id="endDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="End Date" />
-        </div>
-        {/* This is the work-related input area */}
-        <div className={ (this.state.travelReason != "work") ? "hidden" : null }>
-          <label htmlFor="companyInput">Company Name</label><input id="companyInput" type="text" placeholder="Company Name"/>
-        </div>
-        <button className="submitTravelButton" onClick={ this.submitTravel }>Add Travel</button>
       </div>
     );
   }
