0c53ee991dd93ae7df4fceb8dcd4048b0d077ece
bergenhem <cbergenhem@gmail.com>
2016-08-04 21:39:12 -0400

    Add CSS for inputs and input area
    Also add logic for placeholders on input types of date

--- scripts/components/Travel.js
+++ scripts/components/Travel.js
@@ -8,13 +8,23 @@ const Travel = React.createClass({
   reasonChange: function(event) {
     this.setState({ travelReason: event.target.value });
   },
+  changeType: function(event) {
+    var currentType = event.target.type;
+    var currentTarget = event.target;
+    if(currentType === "text") {
+      currentTarget.type = "date";
+    }
+    else if(currentType === "date") {
+        currentTarget.type = "text";
+    }
+  },
   submitTravel: function(event) {
     console.log("add item");
     console.log(event.target);
   },
   render() {
     return(
-      <div>
+      <div className="travelArea">
         <div className="radioButtonArea">
           <div className="travelRadioButton">
             <input type="radio" name="travelType" value="work"
@@ -29,8 +39,14 @@ const Travel = React.createClass({
             Personal
           </div>
         </div>
-        <div className={ (this.state.travelReason === "personal") ? "hidden" : null }>
-          <h1>Display or hide me</h1>
+        {/* This is the personal-related input area */}
+        <div className="dateInputArea">
+          <label htmlFor="startDate">Start Date</label><input id="startDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="Start Date" />
+          <label htmlFor="endDate">End Date</label><input id="endDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="End Date" />
+        </div>
+        {/* This is the work-related input area */}
+        <div className={ (this.state.travelReason != "work") ? "hidden" : null }>
+          <label htmlFor="companyInput">Company Name</label><input id="companyInput" type="text" placeholder="Company Name"/>
         </div>
         <button className="submitTravelButton" onClick={ this.submitTravel }>Add Travel</button>
       </div>
