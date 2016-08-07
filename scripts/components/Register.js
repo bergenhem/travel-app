c58fb4bd5f7b39a97979ba33bdfbb697c045221d
bergenhem <cbergenhem@gmail.com>
2016-08-07 13:33:57 -0400

    Add basic UX for registration

--- scripts/components/Register.js
+++ scripts/components/Register.js
@@ -2,9 +2,24 @@ import React from "react";
 import { render } from "react-dom";
 
 const Register = React.createClass({
+  register: function(event) {
+    event.preventDefault();
+    this.refs.registerForm.reset();
+  },
   render() {
     return(
-      <h1>Register</h1>
+      <div className="overallArea">
+        <div className="overallHeader">
+          <h1>Register</h1>
+        </div>
+        <form className="overallContentArea" ref="registerForm" onSubmit={ this.register }>
+          <input type="text" ref="emailInput" placeholder="Email address" />
+          <input type="text" ref="firstName" placeholder="First Name" />
+          <input type="text" ref="lastName" placeholder="Last Name" />
+          <input type="password" ref="password" placeholder="Password" />
+          <button>Register</button>
+        </form>
+      </div>
     )
   }
 });
