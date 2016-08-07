ef4190f637f78b31b70fbda69b784869b973baed
bergenhem <cbergenhem@gmail.com>
2016-08-06 22:17:26 -0400

    Add basic add functionality with Firebase

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 
 import Rebase from "re-base";
-var base = Rebase.createClass("https://travel-app-1e574.firebaseio.com/");
+import Firebase from "firebase";
+//var base = Rebase.createClass("https://travel-app-1e574.firebaseio.com/");
+
+var config = {
+  apiKey: "3RjI1jSJ9KXAcNP26NB4i6xgjFcXU9SjoqNHIWcC",
+  databaseURL: "https://travel-app-1e574.firebaseio.com/"
+};
+Firebase.initializeApp(config);
+var database = Firebase.database();
+
 // Import components
 import Travel from "./Travel";
 
@@ -13,14 +22,10 @@ const App = React.createClass({
     };
   },
   componentDidMount: function() {
-    base.syncState("/travelItems", {
-      context: this,
-      state: "travelItems",
-      then: function(e) {
-        console.log("then");
-        console.log(e);
-      }
-    });
+    // base.syncState("/travelItems", {
+    //   context: this,
+    //   state: "travelItems"
+    // });
     var localStorageRef = localStorage.getItem("travelItems");
     if(localStorageRef) {
       this.setState({
@@ -32,9 +37,24 @@ const App = React.createClass({
     localStorage.setItem("travelItems", JSON.stringify(nextState));
   },
   addTravelItem: function(travelItem) {
-    var timestamp = (new Date()).getTime();
-    this.state.travelItems["travel-item-" + timestamp] = travelItem;
-    this.setState({ travelItems : this.state.travelItems });
+    var newPostKey = database.ref("/travelItems").push().key;
+    var newItem = {};
+    newItem["/travelItems/" + newPostKey] = travelItem;
+    var test = database.ref().update(newItem);
+    console.log("addTravelItem");
+    console.log(test);
+//     // Get a key for a new Post.
+// var newPostKey = firebase.database().ref().child('posts').push().key;
+//
+// // Write the new post's data simultaneously in the posts list and the user's post list.
+// var updates = {};
+// updates['/posts/' + newPostKey] = postData;
+// updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+//
+// return firebase.database().ref().update(updates);
+    //var timestamp = (new Date()).getTime();
+    //this.state.travelItems["travel-item-" + timestamp] = travelItem;
+    //this.setState({ travelItems : this.state.travelItems });
   },
   render() {
     return (
