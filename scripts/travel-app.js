73dd7c3d3b814e1d0b061101981ce9b8c1c880af
bergenhem <cbergenhem@gmail.com>
2016-08-07 11:25:48 -0400

    Add react-router and history packages

--- scripts/travel-app.js
+++ scripts/travel-app.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Route } from "react-router";
+import { createHistory } from "history";
 
 // Import components
 import App from "./components/App";
