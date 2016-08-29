74a57f4d1a45a4b68588b142641c743a8fc91f36
bergenhem <cbergenhem@gmail.com>
2016-08-29 16:46:35 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit dcb886f4f8358bd0331681b0ef523b91fd5076f2, reversing
    changes made to 89e731b9421b2af5dd7ebcd3342566faafd1010d.

--- scripts/components/Login.js
+++ scripts/components/Login.js
@@ -1,40 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-import AuthHelper from "./helpers/auth";
 
-const Login = React.createClass({
-  login: function(event) {
-    event.preventDefault();
-
-    var email = this.refs.userInput.value;
-    var password = this.refs.passwordInput.value;
-
-    var login = AuthHelper.login(email, password, this.props.createNotification, this.props.router);
-
-    this.refs.loginForm.reset();
-  },
+const Login React.createClass({
   render() {
     return (
-      <div className="overallArea">
-        <div className="overallHeader">
-          <h1>Log in</h1>
-        </div>
-        <form className="overallContentArea" ref="loginForm" onSubmit={ this.login }>
-          <input type="text" ref="userInput" placeholder="Username" />
-          <input type="password" ref="passwordInput" placeholder="Password" />
-          <button>Log in</button>
-          <div className="helperLinks">
-            <span className="alignLeft">
-              <a href="/recovery">Forgot your password?</a>
-            </span>
-            <span className="alignRight">
-              <a href="/register">Register</a>
-            </span>
-          </div>
-        </form>
-      </div>
+      <h1>Log me in</h1>
     )
   }
 });
-
-export default Login;
--- scripts/components/Travel.js
+++ scripts/components/Travel.js
@@ -6,6 +6,7 @@ const Travel = React.createClass({
     return { travelReason: "work" }
   },
   reasonChange: function(event) {
+    console.log("change");
     this.setState({ travelReason: event.target.value });
   },
   changeType: function(event) {
@@ -39,11 +40,11 @@ const Travel = React.createClass({
   // Design for the switch inspired by: http://codepen.io/kylephillips/pen/MYwXqV
   render() {
     return(
-      <div className="overallArea">
-        <div className="overallHeader">
+      <div className="travelArea">
+        <div className="travelHeader">
           <h1>Add Travel</h1>
         </div>
-        <form className="overallContentArea" ref="travelForm" onSubmit={ this.submitTravel }>
+        <form className="travelContentArea" ref="travelForm" onSubmit={ this.submitTravel }>
           <div className="radioButtonArea">
             <div className="switch">
               <div className="travelRadioButton">
@@ -62,7 +63,7 @@ const Travel = React.createClass({
             </div>
           </div>
           {/* This is the personal-related input area */}
-          <div>
+          <div className="dateInputArea">
             <input ref="startDate" id="startDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="Start Date" />
             <input ref="endDate" id="endDate" type="text" onFocus={ this.changeType } onBlur={ this.changeType } placeholder="End Date" />
           </div>
@@ -70,7 +71,7 @@ const Travel = React.createClass({
           <div className={ (this.state.travelReason != "work") ? "hidden" : null }>
             <input ref="companyName" id="companyInput" type="text" placeholder="Company Name"/>
           </div>
-          <button>Add Travel</button>
+          <button className="submitTravelButton">Add Travel</button>
         </form>
       </div>
     );
